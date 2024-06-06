dead8251e404bff3e0edc68198c54bb0e44efba1
yzrofiq <yzrofiq@gmail.com>
2024-06-06 13:05:45 +0700

    tstst

--- manajemenkafe/editmenu.php
+++ manajemenkafe/editmenu.php
@@ -4,8 +4,7 @@ if (!isset($_SESSION['id'])) {
     echo "<script>window.location.href = 'login.html';</script>";
     exit();
 }
-<p>
-<p>
+
 if (isset($_GET['id'])) {
     $menu_id = $_GET['id'];
 
